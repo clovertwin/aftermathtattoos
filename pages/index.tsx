39bb1a97bb1f3861a3baadb734105ed084007551
Shaun Carr <shaunc138@gmail.com>
2022-09-15 20:40:38 -0400

    First test commit to vercel server

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen w-full">
-      <h1 className="text-4xl font-bold md:text-7xl lg:text-8xl">
+    <div className="flex flex-col justify-center items-center h-screen w-full bg-zinc-800">
+      <h1 className="text-4xl font-bold text-neutral-50 md:text-7xl lg:text-8xl">
         Aftermath Tattoos
       </h1>
     </div>
