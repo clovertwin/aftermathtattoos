fc18ef370a5c21d9f5d1dc4367421d12862badc5
Shaun Carr <shaunc138@gmail.com>
2022-09-16 00:14:43 -0400

    Testing fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,19 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen w-full bg-zinc-800">
-      <h1 className="text-4xl font-bold text-neutral-50 md:text-7xl lg:text-8xl">
+    <div className="flex flex-col text-center justify-center items-center h-screen w-full px-4 bg-zinc-800">
+      <h1 className="text-7xl font-header font-bold text-neutral-50 md:text-8xl lg:text-8xl">
         Aftermath Tattoos
       </h1>
+      <p className="text-lg font-sans text-neutral-50">(header font)</p>
+      <h2 className="mt-10 text-4xl font-serif text-neutral-50 md:text-5xl">
+        Strive for Greatness
+      </h2>
+      <p className="text-lg font-sans text-neutral-50">(sub-header font)</p>
+      <p className="mt-10 text-lg font-sans text-neutral-50 md:text-xl">
+        This is a font for chunks of wrinting that will look good small.
+      </p>
+      <p className="text-lg font-sans text-neutral-50">(body font)</p>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        header: ["Almendra Display", "cursive"],
+        serif: ["EB Garamond", ...defaultTheme.fontFamily.serif],
+        sans: ["Quicksand", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
