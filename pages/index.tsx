680622b716884ded6c5daaa28c08930d94036971
Shaun Carr <shaunc138@gmail.com>
2022-10-23 13:32:52 -0400

    Center landing text on small screens

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <>
-      <div className="flex flex-col px-4 font-header text-6xl text-neutral-100 bg-neutral-900 font-bold justify-center items-center h-screen sm:text-8xl md:ml-80 lg:text-9xl">
+      <div className="flex flex-col px-4 font-header text-6xl text-neutral-100 bg-neutral-900 font-bold justify-center items-center h-[93vh] sm:text-8xl md:h-screen md:ml-80 lg:text-9xl">
         <p>Aftermath</p>
         <p>Tattoos</p>
       </div>
