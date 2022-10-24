d1f9c27e028fac6226f0f9624667a5d9ecd82729
Shaun Carr <shaunc138@gmail.com>
2022-10-24 00:49:14 -0400

    adjust gallery layout, scroll

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -2,11 +2,16 @@ import Image from "next/image";
 
 const Gallery = () => {
   return (
-    <section id="gallery" className="bg-neutral-900 md:ml-80">
-      <h2 className="text-5xl font-bold text-center text-neutral-100">
-        Gallery
-      </h2>
-      <div className="mt-4 px-4 py-4 bg-neutral-900 grid grid-cols-1 auto-rows-auto gap-5 place-content-center lg:grid-cols-2">
+    <section
+      id="gallery"
+      className="bg-neutral-900 md:ml-80 scroll-m-20 md:scroll-m-0"
+    >
+      <div className="border-b-4 border-neutral-100">
+        <h2 className="text-5xl font-bold text-neutral-100 lg:text-6xl">
+          Gallery
+        </h2>
+      </div>
+      <div className="mt-4 px-4 py-4 bg-neutral-900 grid grid-cols-1 auto-rows-auto gap-5 lg:grid-cols-2 xl:grid-cols-3">
         <div className="relative rounded-lg border-2 border-neutral-100 w-full max-w-[400px] h-[500px] bg-neutral-900 overflow-hidden justify-self-center">
           <Image
             alt="img1"
