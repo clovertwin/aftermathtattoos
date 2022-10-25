e9fa710e740a5f52bf202f629608f62d8a3e646e
Shaun Carr <shaunc138@gmail.com>
2022-10-25 11:57:35 -0400

    Add galleryimage zoom modal

--- components/GalleryImage.tsx
+++ components/GalleryImage.tsx
@@ -1,11 +1,84 @@
+import { useState, Fragment } from "react";
 import Image from "next/image";
+import { Dialog, Transition } from "@headlessui/react";
 import { TattooImg } from "../lib/types";
 
 const GalleryImage = ({ alt, path }: TattooImg) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => setIsOpen(true);
+
+  const closeModal = () => setIsOpen(false);
+
   return (
-    <div className="relative rounded-lg border-2 border-neutral-100 w-full max-w-[200px] h-[230px] bg-neutral-900 overflow-hidden justify-self-center lg:max-w-[300px] lg:h-[370px]">
-      <Image alt={alt} src={path} layout="fill" objectFit="cover" />
-    </div>
+    <>
+      <div
+        onClick={openModal}
+        className="relative rounded-lg border-2 border-neutral-100 w-full max-w-[200px] h-[230px] bg-neutral-900 overflow-hidden justify-self-center lg:max-w-[300px] lg:h-[370px]"
+      >
+        <Image alt={alt} src={path} layout="fill" objectFit="cover" />
+      </div>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-30" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-sm transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all md:max-w-md">
+                  <Dialog.Title
+                    as="div"
+                    className="relative flex justify-end items-center z-40"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-10 h-10 text-neutral-100 ring ring-neutral-100"
+                      onClick={closeModal}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </Dialog.Title>
+                  <div className="w-full max-w-sm h-[400px] md:max-w-md md:h-[500px]">
+                    <Image
+                      alt={alt}
+                      src={path}
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
   );
 };
 
