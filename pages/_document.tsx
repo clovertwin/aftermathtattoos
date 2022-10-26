f967b1db99598961f43e899ba13e761cbf4c92fa
Shaun Carr <shaunc138@gmail.com>
2022-10-26 11:42:38 -0400

    remove excess link tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,11 +15,6 @@ export default function Document() {
           rel="stylesheet"
         />
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
           rel="icon"
           type="image/png"
           sizes="32x32"
@@ -31,7 +26,6 @@ export default function Document() {
           sizes="16x16"
           href="/images/favicon-16x16.png"
         />
-        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
       <body className="bg-neutral-900">
         <Main />
